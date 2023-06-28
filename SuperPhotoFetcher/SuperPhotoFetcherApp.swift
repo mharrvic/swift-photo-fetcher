7f3e6b505c7f9df46afd821d3e9a87c134840115
Mac Chic <mharrvic@gmail.com>
2023-06-28 15:59:55 +0800

    Initial Commit

---
+++ SuperPhotoFetcher/SuperPhotoFetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperPhotoFetcherApp.swift
+//  SuperPhotoFetcher
+//
+//  Created by mac on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperPhotoFetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
